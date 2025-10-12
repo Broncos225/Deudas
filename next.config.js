c2de3f3bc8a6ee2a40d385d9a3e36a4868850e27
Andrés Yepes <102176786+Broncos225@users.noreply.github.com>
2025-10-11 21:25:16 -0500

    Act NextJS

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
-
 const nextConfig = {
   reactStrictMode: true,
   // This is a new security feature in Next.js 15.
   // We need to allow requests from the preview server to the dev server.
   // https://nextjs.org/docs/app/api-reference/next-config-js/allowedDevOrigins
-  experimental: {
-    // allowedDevOrigins has been promoted from experimental. It's now a top-level property.
-  },
   allowedDevOrigins: ["https://*.cloudworkstations.dev"],
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
